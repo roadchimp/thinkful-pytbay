af56bcd6e6db0a4a48aff87cc9a10e4235ecb0e1
Sam Sena <samsena@gmail.com>
2015-03-17 13:41:12 +0000

    Testing SQL Alchemy commands

--- tbay.py
+++ tbay.py
@@ -28,3 +28,28 @@ class Bid(Base):
   price = Column(Float, nullable=False)
   
 Base.metadata.create_all(engine)
+
+#creates a user
+# beyonce = User()
+# beyonce.username = "bknowles"
+# beyonce.password = "crazyinlove"
+# session.add(beyonce)
+# session.commit()
+
+# Returns a list of all of the user objects
+session.query(User).all() # Returns a list of all of the user objects
+
+# Returns the first user
+session.query(User).first()
+
+# Finds the user with the primary key equal to 1
+session.query(User).get(1)
+
+# Returns a list of all of the usernames in ascending order
+session.query(User.username).order_by(User.username).all()
+
+# Returns the description of all of the basesballs
+session.query(Item.description).filter(Item.name == "baseball").all()
+
+# Return the item id and description for all baseballs which were created in the past.  Remember to import the datetime object: from datetime import datetime
+session.query(Item.id, Item.description).filter(Item.name == "baseball", Item.start_time < datetime.utcnow()).all()
